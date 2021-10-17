b7f8f3335a9f8822d719730a52de3ab4e8297cac
Mykola-Veres <veresnickolay@gmail.com>
2021-10-17 17:48:16 +0300

    Update-Task-10

--- js/task-10.js
+++ js/task-10.js
@@ -17,11 +17,9 @@ buttonCreate.addEventListener("click", () => {
 buttonDestroy.addEventListener("click", () => {
   boxesAll.innerHTML = "";
   size = 30;
-
 })
-
-controlsElement.addEventListener("change", (event) => {
-  let inputAsNumber = event.target.valueAsNumber;
+// controlsElement.addEventListener("change", (event) => {
+//   let inputAsNumber = event.target.valueAsNumber;
   // if (buttonCreate.addEventListener("click", createBoxes(inputAsNumber)))
   // {createBoxes(inputAsNumber)} else
   // if (buttonDestroy.addEventListener("click", () => {
@@ -29,26 +27,16 @@ controlsElement.addEventListener("change", (event) => {
   //   inputElement.value = ""
   //   boxesAll.remove();
   // }));       
-});
+// });
 function createBoxes(amount) {
-   
   for (let i = 0; i < amount; i += 1) {
     size += 10;
     const divElement = document.createElement("div");
     divElement.style.height = `${size}px`;
     divElement.style.width = `${size}px`;
     divElement.classList = "item";
-    divElement.style.backgroundColor = getRandomHexColor();
-    console.log("🚀divElement", divElement);    
-    // divElement[i].style.height;
+    divElement.style.backgroundColor = getRandomHexColor();       
     boxesAll.appendChild(divElement);
-    // divAllElement.push(divElement);    
 };
-console.log("divAllElement", divAllElement);
-// const All = divAllElement.reduce((acc, el)=>  )
-
-boxesAll.append(...divAllElement);
+// boxesAll.append(...divAllElement);
 };
-
-// event.currentTarget.elements
-
